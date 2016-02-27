f26885de8da69f8539ebb116e925da0f220d28b9
Bishop Lafer <bishoplafer@Bishops-MacBook-Pro.local>
2016-02-27 15:22:18 -0800

    immutable_js

--- voting-server/test/immutable_spec.js
+++ voting-server/test/immutable_spec.js
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import{List} from 'immutable';
+import{List, Map} from 'immutable';
 
 describe('immutability', () => {
 
@@ -8,22 +8,28 @@ describe('immutability', () => {
 	describe('A List', () => {
 
 		function addMovie(currentState, movie) {
-			return currentState.push(movie);
+			return currentState.update('movies', movies => movies.push(movie));
 		}
 
 		it('is immutable', () => {
-			let state = List.of('Trainspotting', '28 Days Later');
+			let state = Map({
+				movies: List.of('Trainspotting', '28 Days Later')
+			});
 			let nextState = addMovie(state, 'Sunshine');
 
-			expect(nextState).to.equal(List.of(
-				'Trainspotting',
-				'28 Days Later',
-				'Sunshine'
-			));
-			expect(state).to.equal(List.of(
-				'Trainspotting',
-				'28 Days Later'
-			));
+			expect(nextState).to.equal(Map({
+				movies: List.of(
+					'Trainspotting',
+					'28 Days Later',
+					'Sunshine'
+				)
+			}));
+			expect(state).to.equal(Map({
+				movies: List.of(
+					'Trainspotting',
+					'28 Days Later'
+				)
+			}));
 		});
 
 	});
