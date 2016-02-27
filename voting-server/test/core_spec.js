b53e5e0a817708cb1a023ac8aa2efd37b9003346
Bishop Lafer <bishoplafer@Bishops-MacBook-Pro.local>
2016-02-27 15:38:18 -0800

    vote

--- voting-server/test/core_spec.js
+++ voting-server/test/core_spec.js
@@ -5,14 +5,20 @@ import {setEntries} from '../src/core';
 
 describe('application logic', () => {
 
-  describe('setEntries', () => {
+  // ..
 
-    it('adds the entries to the state', () => {
-      const state = Map();
-      const entries = List.of('Trainspotting', '28 Days Later');
-      const nextState = setEntries(state, entries);
+  describe('next', () => {
+
+    it('takes the next two entries under vote', () => {
+      const state = Map({
+        entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+      });
+      const nextState = next(state);
       expect(nextState).to.equal(Map({
-        entries: List.of('Trainspotting', '28 Days Later')
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        }),
+        entries: List.of('Sunshine')
       }));
     });
 
