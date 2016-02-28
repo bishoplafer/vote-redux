bd314c24f65923d28cd3cce18aebd1c2dc865f43
Bishop Lafer <bishoplafer@Bishops-MacBook-Pro.local>
2016-02-27 16:00:40 -0800

    vote

--- voting-server/test/core_spec.js
+++ voting-server/test/core_spec.js
@@ -1,11 +1,20 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
-
-import {setEntries} from '../src/core';
+import {setEntries, next, vote} from '../src/core';
 
 describe('application logic', () => {
 
-  // ..
+  describe('setEntries', () => {
+
+    it('adds the entries to the state', () => {
+      const state = Map();
+      const entries = List.of('Trainspotting', '28 Days Later');
+      const nextState = setEntries(state, entries);
+      expect(nextState).to.equal(Map({
+        entries: List.of('Trainspotting', '28 Days Later')
+      }));
+    });
+  });
 
   describe('next', () => {
 
@@ -21,7 +30,51 @@ describe('application logic', () => {
         entries: List.of('Sunshine')
       }));
     });
+  });
 
+  describe('vote', () => {
+
+    it('creates a tally for the voted entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        }),
+        entries: List()
+      });
+      const nextState = vote(state, 'Trainspotting');
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting' : 1
+          })
+        }),
+      }));
+    });
+
+    it('adds to existing tally for the voted entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting' : 3,
+            '28 Days Later' : 2
+          })
+        }),
+        entries: List()
+      });
+      const nextState = vote(state, 'Trainspotting');
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting' : 4,
+            '28 Days Later' : 2
+          })
+        }),
+        entries: List()
+      }));
+    });
   });
 
 });
